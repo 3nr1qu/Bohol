b30a42d71a828e7ca043eb7282c8309f33bf23ff
EnriqueSiarot <enrique.siarot@student.passerellesnumeriques.org>
2022-09-08 11:20:07 +0800

    add all

---
+++ public/js/alert.js
@@ -0,0 +1,5 @@
+function message() { 
+    var fullname=document.getElementById("fullname").value 
+    var email=document.getElementById("email").value 
+    alert("Thank you for messaging us "+fullname+"! We'll send you a response to your email: "+email+".")
+}
